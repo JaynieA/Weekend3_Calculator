b9007ab6d6279173c48d826ea7085910f4cf7c68
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2016-12-10 00:26:58 -0600

    displaying inputs on dom, created more functioning for easier readability

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,5 @@
+var logs = false;
+
 var Operation = function(x, y, type) {
   this.x = x;
   this.y = y;
@@ -5,26 +7,29 @@ var Operation = function(x, y, type) {
 }; // end Operation
 
 var displayResult = function(number){
-  console.log('in displayResult');
+  if (logs) console.log('in displayResult');
   if (number === undefined) {
     //display message if no numbers have been entered when = is clicked
-    $('#result').html('<p class="text-center result">Calculator cannot calculate.</p>');
+    $('#display').html('<p class="text-center display">Calculator cannot calculate.</p>');
   } else {
-    $('#result').html('<p class="text-center result">= ' + number + '</p>');
+    $('#display').html('<p class="text-center display">' + number + '</p>');
   } // end else
 }; // end displayResult
 
 var getInputNumbers = function(object, property, number) {
-  console.log('in createInputNumber');
+  if (logs) console.log('in createInputNumber');
   if (object[property] === undefined) {
     object[property] = number;
+    // $('#display').html('<p class="text-center display">' + object[property] + '</p>');
+    displayResult(object[property]);
   } else {
     object[property] = object[property] + number;
+    displayResult(object[property]);
   } // end else
 }; // end createInputNumber
 
 var init = function() {
-  console.log('in init');
+  if (logs) console.log('in init');
   //create operation object
   var operation = new Operation();
   //event listeners
@@ -32,21 +37,21 @@ var init = function() {
     numberClicked = $(this).text();
     if (operation.type === undefined) {
       getInputNumbers(operation, 'x', numberClicked);
+      // $('#display').html(operation.x);
     } else {
       getInputNumbers(operation, 'y', numberClicked);
+      // $('#display').html(operation.y);
     } // end else
-    console.log('.btn-num Clicked:',operation);
+    if (logs) console.log('.btn-num Clicked:',operation);
+
   }); // end .btn-num onclick
   $('.btn-type').on('click', function() {
     operation.type = $(this).text();
-    console.log('opeartion after .btn-type', operation);
+    // if (logs) console.log('opeartion after .btn-type', operation);
   }); // end .btn-type onclick
-  $('#submit').on('click', function() {
-    postOperation(operation);
-    //clear input and select fields
-    $('input').val('');
-    $('select').children().first().prop('selected', true);
-  }); // end #submit onclick
+  $('#submit').on('click', function(){
+    submit(operation);
+  });
   $('#clear').on('click', function() {
     reset(operation);
   }); // end #clear onclick
@@ -58,26 +63,34 @@ var postOperation = function(object) {
     url: '/',
     data: object,
     success: function(response) {
-      console.log('ajax post success. Response:', response);
+      if (logs) console.log('ajax post success. Response:', response);
       displayResult(response.result);
     },
     error: function() {
-      console.log('ajax error on post');
+      if (logs) console.log('ajax error on post');
     }
   }); // end ajax post
 }; // end postOperation
 
 var reset = function(object) {
-  console.log('in reset');
+  if (logs) console.log('in reset');
   //clear result displayed on DOM
-  $('#result').html('');
+  $('#display').html('');
   //clear values of operation
   object.x = undefined;
   object.y = undefined;
   object.type = undefined;
   //TODO: RESET THE RESULT
-  console.log('operation after reset:', object);
+  if (logs) console.log('operation after reset:', object);
 }; // end reset
+
+var submit = function(object) {
+  if (logs) console.log('in submit');
+  postOperation(object);
+  //clear input and select fields
+  $('input').val('');
+  $('select').children().first().prop('selected', true);
+}; // end submit
 
 $(document).ready(function() {
   init();
