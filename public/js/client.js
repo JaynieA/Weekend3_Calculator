ce6fe31e3b8b3d3b34be7872360184c61ced54c6
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2016-12-09 20:30:16 -0600

    set up get and post calls to server to test. working.

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-  console.log('JQ');
-});
+
+  $.ajax({
+    type: 'POST',
+    url: '/',
+    data: {animal: "dog"},
+    success: function(response) {
+      console.log('ajax post success. Response:', response);
+    },
+    error: function() {
+      console.log('ajax error on post');
+    }
+  }); // end ajax post
+
+  $.ajax({
+    type: 'GET',
+    url: '/get',
+    success: function(response) {
+      console.log('ajax get success. Response:', response);
+    },
+    error: function() {
+      console.log('ajax error on get');
+    }
+  }); // end ajax get
+
+}); // end doc ready
--- server/app.js
+++ server/app.js
@@ -1,14 +1,22 @@
+var PORT = 3000;
 var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
-
-var PORT = 3000;
-
-//moderators??
+//middleware
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.listen(PORT, function() {
   console.log('server listening on port', PORT);
+}); // end app listen
+
+app.post('/', function(req, res) {
+  console.log(req.body);
+  res.send(req.body);
+}); // end app post
+
+app.get('/get', function(req, res) {
+  console.log('get landed on server');
+  res.send('Hello');
 });
