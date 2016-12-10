9eb9a949ec03c92a4be5726ef2a0893590154c7f
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2016-12-09 20:14:25 -0600

    path set to public

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
+var bodyParser = require( 'body-parser' );
 
 var PORT = 3000;
 
 //moderators??
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.listen(PORT, function() {
   console.log('server listening on port', PORT);
